25bd52aa6a10133990e435bbb0a0db62f563e1f5
papontemjetix <papontem@gmail.com>
2023-07-15 16:24:30 -0400

    progress on part1-part4

--- soccer_leargues.sql
+++ soccer_leargues.sql
@@ -18,33 +18,60 @@ CREATE DATABASE soccer_leagues_db;
 
 CREATE TABLE leagues 
 (
-    id SERIAL PRIMARY KEY
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    season_id INT REFERENCES seasons (id) ON DELTE SET NULL
 
 );
+
 CREATE TABLE seasons 
 (
-    id SERIAL PRIMARY KEY
+    id SERIAL PRIMARY KEY,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL
 );
+
 CREATE TABLE teams 
 (
-    id SERIAL PRIMARY KEY
+    id SERIAL PRIMARY KEY,
+    team_name TEXT,
+    league_id INTEGER REFERENCES leagues (id) ON DELETE SET NULL
 );
+
 CREATE TABLE players 
 (
-    id SERIAL PRIMARY KEY
+    id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    team_id INT REFERENCES teams (id) ON DELETE SET NULL,
+    position TEXT 
+
 );
+
 CREATE TABLE referees 
 (
-    id SERIAL PRIMARY KEY
+    id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL
 
 );
+
 CREATE TABLE matches 
 (
-    id SERIAL PRIMARY KEY
-
+    id SERIAL PRIMARY KEY,
+    home_team_id INTEGER REFERENCES teams (id),
+    home_score INTEGER,
+    away_team_id INTEGER REFERENCES teams (id),
+    away_score INTEGER,
+    referee_id INTEGER REFERENCES referees (id),
+    match_duration TIME
 );
+
 CREATE TABLE match_goals 
 (
-    id SERIAL PRIMARY KEY
+    id SERIAL PRIMARY KEY,
+    match_id INTEGER REFERENCES matches (id) ON DELETE CASCADE,
+    goal_scorer_player_id INTEGER REFERENCES players (id) ON DELETE NULL,
+    goalie_player_id INTEGER REFERENCES players (id) ON DELETE NULL
 
 );
