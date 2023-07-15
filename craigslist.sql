a4cdeefef0826ced08532eabf34215a8490031de
papontemjetix <papontem@gmail.com>
2023-07-15 12:31:28 -0400

    hmmmmm update on the creation of tables for craiglist

--- craigslist.sql
+++ craigslist.sql
@@ -13,7 +13,32 @@ CREATE DATABASE craigslist_db;
 -- Posts: title, text, user , post location, post region
 -- Categories: that each post belongs to
 
-CREATE TABLE 
-CREATE TABLE 
-CREATE TABLE 
-CREATE TABLE 
+CREATE TABLE regions
+(
+    id SERIAL PRIMARY KEY,
+    region TEXT VARCHAR(100),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE users
+(
+    id SERIAL PRIMARY KEY,
+    username TEXT VARCHAR(25) UNIQUE,
+    user_preffered_region_id INTEGER  REFERENCES regions (id) ON DELETE NULL,
+);
+
+CREATE TABLE posts
+(
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    user_id INTEGER REFERENCES users (id) NOT NULL ON DELETE CASCADE,
+    text_description TEXT, 
+    post_location TEXT,
+    post_region INTEGER REFERENCES regions (id) NOT NULL ON DELETE NULL
+);
+
+CREATE TABLE categories
+(
+    id SERIAL PRIMARY KEY,
+    category TEXT VARCHAR(30) NOT NULL,
+);
